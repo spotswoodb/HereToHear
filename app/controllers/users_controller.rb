6a2c551740cd2744a06506eb8743c385e3b166ba
Ben Spotswood <spotswoodb@gmail.com>
2021-04-07 21:52:53 -0500

    commit tree 13506f0ab984e59197ad24fa9c2b5ff0aa98f336

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+ 
+
     def index
         @users = User.all
     end
@@ -50,5 +52,7 @@ private
   def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
   end
+
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,34 @@
 class User < ApplicationRecord
+  validate :first_name_is_title_case
+  validate :last_name_is_title_case
+
+
   validates :first_name, :last_name, presence: true, length: { minimum: 2 }
   # validates :last_name, presence: true, length: { minimum: 2 }
   validates :email, :username, uniqueness: true, presence: true, length: { minimum: 2 }
   # validates :username, uniqueness: true, presence: true, length: { minimum: 2 }
   validates :password, presence: true, length: { in: 6..15 }
   has_secure_password
+
+  before_validation :make_title_case
+  
+  private
+
+  def first_name_is_title_case
+    if first_name.split.any?{|w| w[0].upcase != w[0]}
+        errors.add(:first_name, "First name must be in Capitalized")
+    end
+  end
+    
+  def last_name_is_title_case 
+      if last_name.split.any?{|w| w[0].upcase != w[0]}
+          errors.add(:last_name, "Last name must be in Capitalized")
+      end
+  end
+
+  def make_title_case
+    self.first_name = self.first_name.titlecase
+    self.last_name = self.last_name.titlecase
+  end
+
 end
